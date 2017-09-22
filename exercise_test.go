6aae7752d21e783297a39a8fb9f3a4e1df4ead0b
pallat <yod.pallat@gmail.com>
2017-09-22 16:33:04 +0700

    test for struct

--- exercise_test.go
+++ exercise_test.go
@@ -1,40 +1,45 @@
 package exercises
 
-import (
-	"reflect"
-	"testing"
-)
+import "testing"
 
-func TestLetterCount(t *testing.T) {
-	sentenses := []string{
-		"I am learning Go!",
-		"The quick brown fox jumped over the lazy dog.",
-	}
+func TestJsonStructure(t *testing.T) {
+	json := `{
+	"id": 1,
+	"lang": "go"		
+}`
 
-	expected := []map[string]int{
-		map[string]int{
-			"I":        1,
-			"am":       2,
-			"learning": 8,
-			"Go!":      3,
-		},
-		map[string]int{
-			"The":    3,
-			"quick":  5,
-			"brown":  5,
-			"fox":    3,
-			"jumped": 6,
-			"over":   4,
-			"the":    3,
-			"lazy":   4,
-			"dog.":   4,
-		},
-	}
+	type testStruct = langStruct
+	var myStruct testStruct
 
-	if !reflect.DeepEqual(LettersCount(sentenses[0]), expected[0]) {
-		t.Error(LettersCount(sentenses[0]), expected[0])
+	err := json.Unmarshal([]byte(json), &myStruct)
+
+	if err != nil {
+		t.Error("it should not error when you make the righ struct that represent json string")
 	}
-	if !reflect.DeepEqual(LettersCount(sentenses[1]), expected[1]) {
-		t.Error(LettersCount(sentenses[1]), expected[1])
+}
+
+func TestJsonStructures(t *testing.T) {
+	json := `[
+	{
+		"id": 1,
+		"lang": "go"
+	},
+	{
+		"id": 2,
+		"lang": "java"
+	},
+	{
+		"id": 3,
+		"lang": "python"
+	}
+]`
+
+	type testStruct = langStruct
+	var myStruct []testStruct
+
+	err := json.Unmarshal([]byte(json), &myStruct)
+
+	if err != nil {
+		t.Error("it should not error when you make the righ struct that represent json string")
 	}
 }
