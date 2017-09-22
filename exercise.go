8f4fc55f3887f5024f367f1d1e4cdbae0a0bf08b
pallat <yod.pallat@gmail.com>
2017-09-22 16:40:23 +0700

    exercise to calculate Pi

---
+++ exercise.go
@@ -0,0 +1,11 @@
+package golang
+
+// formula: π = (4/1) - (4/3) + (4/5) - (4/7) + (4/9) - (4/11) + (4/13) - (4/15) ...
+
+func odds(f float64) []float64 {
+	return []float64{}
+}
+
+type odde struct {
+	f float64
+}
