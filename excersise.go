3b50f6dc4bcf2d18c781e11adec71f4d754d0be4
obbaeiei <ob.akesiripong@gmail.com>
2017-10-21 13:26:31 +0700

    fix

--- excersise.go
+++ excersise.go
@@ -2,8 +2,8 @@ package golang
 
 func factorial(num int) int {
 	var result = 1
-	for i := 0; i < num; i++ {
-		result = result * (num - i)
+	for ; 0 < num; num-- {
+		result = result * num
 	}
 	return result
 }
