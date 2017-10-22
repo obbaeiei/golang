600d6d50f3ced34905b1254aa34ab30ffb21ed5a
obbaeiei <ob.akesiripong@gmail.com>
2017-10-22 10:02:37 +0700

    add test case 04 then passed

--- exercise_test.go
+++ exercise_test.go
@@ -25,3 +25,11 @@ func TestCase3GetFizz(t *testing.T) {
 		t.Errorf("%s is expected result but got %s", expected, result)
 	}
 }
+
+func TestCase4Get4(t *testing.T) {
+	result := text(4)
+	expected := "4"
+	if result != expected {
+		t.Errorf("%s is expected result but got %s", expected, result)
+	}
+}
