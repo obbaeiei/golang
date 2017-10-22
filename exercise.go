ccbf471fe6143d0e0a74d422638037824e090c6b
obbaeiei <ob.akesiripong@gmail.com>
2017-10-22 09:24:28 +0700

    done 12

--- exercise.go
+++ exercise.go
@@ -1,9 +1,19 @@
 package golang
 
-func enc(xx, rot byte) byte {
-	return xx
+func enc(x rune, rot byte) rune {
+	return ((int32(x) + int32(rot)) % 26) + (int32([]rune("a")[0]))
 }
 
+//
 func caesar(s string, rot byte) string {
-	return s
+	var result string
+	pieceOfString := []rune(s)
+	for _, v := range pieceOfString {
+		if v != int32((int32([]rune(" ")[0]))) {
+			result += string(enc(v, rot))
+		} else {
+			result += " "
+		}
+	}
+	return result
 }
