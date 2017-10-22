e981aeffea8b4629caf632e10f03dd704abad669
obbaeiei <ob.akesiripong@gmail.com>
2017-10-22 09:31:26 +0700

    remove not exercise_tdd

--- exercise_test.go
+++ exercise_test.go
@@ -4,12 +4,7 @@ import (
 	"testing"
 )
 
-func TestCaesarCipherROT4(t *testing.T) {
-	original := "the quick brow fox jumps over the lazy dog"
-	expected := "qeb nrfzh yolt clu grjmp lsbo qeb ixwv ald"
-
-	s := caesar(original, 4)
-
+func Test(t *testing.T) {
 	if s != expected {
 		t.Errorf("%s is expected result but got %s", expected, s)
 	}
