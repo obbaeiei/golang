784f8582f401e6d154cb6ebb8d1a429e21f0197f
obbaeiei <ob.akesiripong@gmail.com>
2017-10-22 09:59:53 +0700

    add testcase 2

--- exercise_test.go
+++ exercise_test.go
@@ -5,7 +5,14 @@ import "testing"
 func TestCase1Get1(t *testing.T) {
 	result := text(1)
 	expected := "1"
+	if result != expected {
+		t.Errorf("%s is expected result but got %s", expected, result)
+	}
+}
 
+func TestCase2Get2(t *testing.T) {
+	result := text(2)
+	expected := "2"
 	if result != expected {
 		t.Errorf("%s is expected result but got %s", expected, result)
 	}
